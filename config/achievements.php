dd1d4b2f6ecf403ee4f430e7dbfbb178824dca30
Behzad Fazelasl <behzad.fazelasl@gmail.com>
2021-08-05 00:26:06 +0300

    Add config file for Achievements and Badges

---
+++ config/achievements.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'lesson_watched' => [
+        'First Lesson Watched' => ['number_of_lessons_to_unlock' => 1],
+        '5 Lessons Watched' => ['number_of_lessons_to_unlock' => 5],
+        '10 Lessons Watched' => ['number_of_lessons_to_unlock' => 10],
+        '25 Lessons Watched' => ['number_of_lessons_to_unlock' => 25],
+        '50 Lessons Watched' => ['number_of_lessons_to_unlock' => 50]
+    ],
+    'comment_written' => [
+        'First Comment Written' => ['number_of_comments_to_unlock' => 1],
+        '3 Comments Written' => ['number_of_comments_to_unlock' => 3],
+        '5 Comments Written' => ['number_of_comments_to_unlock' => 5],
+        '10 Comments Written' => ['number_of_comments_to_unlock' => 10],
+        '20 Comments Written' => ['number_of_comments_to_unlock' => 20]
+    ]
+
+];
---
+++ config/badges.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'Beginner' => [ 'number_of_achievements_to_unlock' => 0],
+    'Intermediate' => [ 'number_of_achievements_to_unlock' => 4],
+    'Advanced' => [ 'number_of_achievements_to_unlock' => 8],
+    'Master' => [ 'number_of_achievements_to_unlock' => 10] 
+];
